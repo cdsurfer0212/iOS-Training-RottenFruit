93d4b0e2f6c3de12791331718235efbaa415daea
cdsurfer0212 <cdsurfer0212@gmail.com>
2015-06-12 13:03:59 +0800

    Initial Commit

---
+++ RottenFruit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RottenFruit
+//
+//  Created by Sean Zeng on 6/12/15.
+//  Copyright (c) 2015 Yahoo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
